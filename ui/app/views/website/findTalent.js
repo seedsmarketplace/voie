9cd58884cdbe38b169e6b210115a0c3eff70e638
srsindia <bhaskar@srsconsultinginc.com>
2013-03-07 20:07:00 +0530

    new screens for side menue

---
+++ ui/app/views/website/findTalent.js
@@ -0,0 +1,35 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'backboneExt',
+  'utils',
+  'navigation',
+  'models/appState',
+], function($, _, Backbone,backboneExt,utils,navigationIns,appStateIns){
+	
+	var MissionView = backboneExt.SeedsView.extend({
+		
+		name : "FindTalentView",
+		
+		
+		templateName : 'website/findTalent',
+		
+		initialize : function(){
+			_.bindAll(this);
+		},
+		
+		render : function(){
+			this.renderTemplate({
+				appState : appStateIns.toJSON(),
+			});
+			return this;
+		},
+		
+		onRenderComplete : function(){
+		},
+	});
+		
+	return MissionView;
+	  
+});
